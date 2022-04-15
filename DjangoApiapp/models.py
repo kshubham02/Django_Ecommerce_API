40830d10988fa3fb48d01a8f45a778f0097d535d
shubham062000 <join.shubhamtorkade@gmail.com>
2022-04-15 15:15:33 +0530

    first commit

---
+++ DjangoApiapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class CartItem(models.Model):
+    product_name = models.CharField(max_length=200)
+    product_price = models.FloatField()
+    product_quantity = models.PositiveIntegerField()
+    
+    def __str__(self):
+        return self.product_name
+    
---
+++ DjangoApiapp/views.py
@@ -0,0 +1,40 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from .serializers import CartItemSerializer
+from .models import CartItem
+
+# For posting data to the database
+class CartItemViews(APIView):
+    def post(self, request):
+        serializer = CartItemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+        else:
+            return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, id=None):
+        if id:
+            item = CartItem.objects.get(id=id)
+            serializer = CartItemSerializer(item)
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+
+        items = CartItem.objects.all()
+        serializer = CartItemSerializer(items, many=True)
+        return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+    
+    def patch(self, request, id=None):
+        item = CartItem.objects.get(id=id)
+        serializer = CartItemSerializer(item, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data})
+        else:
+            return Response({"status": "error", "data": serializer.errors})
+        
+    def delete(self, request, id=None):
+        item = get_object_or_404(CartItem, id=id)
+        item.delete()
+        return Response({"status": "success", "data": "Item Deleted"})
